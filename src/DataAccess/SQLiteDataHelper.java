a3562337287bb9e9687d948987a67caabc1a0ecc
Dax <daxnavarrete@gmail.com>
2025-01-25 20:42:59 -0500

    fix: sqlited database path

--- src/DataAccess/SQLiteDataHelper.java
+++ src/DataAccess/SQLiteDataHelper.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class SQLiteDataHelper {
-    private static String DBPathConnection = "jdbc:sqlite:database//PoliSalud.sqlite";
+    private static String DBPathConnection = "jdbc:sqlite:database/polisalud.sqlite";
     private static Connection conn = null;
 
     protected SQLiteDataHelper() {
