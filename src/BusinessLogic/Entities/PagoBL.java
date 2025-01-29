99c39cec488a13784ef77ca498d1b5ecf3e67593
Matthew <169124743+Matthrve@users.noreply.github.com>
2025-01-28 21:52:50 -0500

    Add files via upload

---
+++ src/BusinessLogic/Entities/PagoBL.java
@@ -0,0 +1,35 @@
+package BusinessLogic.Entities;
+
+package BusinessLogic;
+
+import java.util.List;
+
+import DataAccess.DAO.PagoDAO;
+import DataAccess.DTO.PagoDTO;
+
+public class PagoBL {
+    private PagoDTO pago;       
+    private PagoDAO pagDAO = new PagoDAO();
+
+    public PagoBL(){}
+
+    public List<PagoDTO> getAll() throws Exception{
+        return pagDAO.readAll();
+    }
+    public PagoDTO getByIdSexo(int idPago) throws Exception{
+        pago = pagDAO.readBy(idPago);
+        return pago;
+    }
+    public boolean create(PagoDTO pagoDTO) throws Exception{   
+        return pagDAO.create(pagoDTO);
+    }
+    public boolean update(PagoDTO pagoDTO) throws Exception{
+        return pagDAO.update(pagoDTO);
+    }
+    public boolean delete(int idPago) throws Exception{
+        return pagDAO.delete(idPago);
+    }
+    public Integer getMaxRow() throws Exception{
+        return pagDAO.getMaxRow();
+    }
+}
