4cddd22bc9d99ef2e278b82c7b1114b923eebe4b
Carolay28 <carolaypillajo@gmail.com>
2025-01-28 22:00:45 -0500

    Add files via upload

---
+++ src/BusinessLogic/Entities/PacienteBL.java
@@ -0,0 +1,32 @@
+package BusinessLogic.Entities;
+
+import DataAccess.DAO.PacienteDAO;
+import DataAccess.DTO.PacienteDTO;
+import java.util.List;
+
+public class PacienteBL {
+    private PacienteDTO paciente;       
+    private PacienteDAO pacDAO = new PacienteDAO();
+
+    public PacienteBL(){}
+
+    public List<PacienteDTO> getAll() throws Exception{
+        return pacDAO.readAll();
+    }
+    public PacienteDTO getByIdPaciente(int idPaciente) throws Exception{
+        paciente = pacDAO.readBy(idPaciente);
+        return paciente;
+    }
+    public boolean create(PacienteDTO pacienteDTO) throws Exception{   
+        return pacDAO.create(pacienteDTO);
+    }
+    public boolean update(PacienteDTO pacienteDTO) throws Exception{
+        return pacDAO.update(pacienteDTO);
+    }
+    public boolean delete(int idPaciente) throws Exception{
+        return pacDAO.delete(idPaciente);
+    }
+    public Integer getMaxRow() throws Exception{
+        return pacDAO.getMaxRow();
+    }
+}
